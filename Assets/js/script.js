156e933dab2c621087b555b1298dcc2a6422d302
Alex Geis <siegxela@gmail.com>
2022-02-11 16:11:31 -0700

    First round commit for JS random password generator HW. Includes index.html, css, JS, and readme files.

---
+++ Assets/js/script.js
@@ -0,0 +1,71 @@
+var lowercaseChar = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var uppercaseChar = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var numericChar = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var specialChar = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '+', '-', '.', '~', '|', '<', '>', '=', '-', '_', '/', ':', ';', '?', '[', ']', '{', '}', '~'];
+// var specialCharAll = " !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+// var specialChar = specialCharAll.split(' ');
+// var characters = Number(character)
+
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+var userPass = [];
+  console.log(userPass);
+var passwordNew = [];
+
+
+function generatePassword() {
+  //1) request/store password length
+  characters = parseInt(prompt("How many characters would you like in your password?"));
+  // console.log(characters);
+  //1.1) restrict character length
+  if (characters < 8 || characters > 128) {
+    alert("Your password character count must be at least 8 characters and no more than 128 characters in length.")
+    generatePassword();
+  };
+  //2.1) Validate lowercase character inclusion
+  var chooseLower = confirm("Do you want to include lowercase characters?")
+  if (chooseLower == true) {
+    userPass = userPass.concat(lowercaseChar);
+  } 
+  //2.2) Validate uppercase character inclusion
+  var chooseUpper = confirm("Do you want to include uppercase characters?")
+  if (chooseUpper == true) {
+    userPass = userPass.concat(uppercaseChar);
+  } 
+  //2.3) Validate numeric character inclusion
+  var chooseNumber = confirm("Do you want to include numbers?")
+  if (chooseNumber == true) {
+    userPass = userPass.concat(numericChar);
+  } 
+  //2.4) Validate special character inclusion
+  var chooseSpecial = confirm("Do you want to include special characters?")
+  if (chooseSpecial == true) {
+    userPass = userPass.concat(specialChar);
+  } 
+  //2.5) Validate a character type was selected
+  if ((chooseLower == false) && 
+    (chooseUpper == false) && 
+    (chooseNumber == false) && 
+    (chooseSpecial == false)
+    ) {
+      alert("You must choose at least 1 character type.");
+      generatePassword();
+    }
+  //loop through final array of potential characters and randomly select characters until the password length is reached
+  for (var i = 0; i < 1 + characters; i++){
+    passwordNew += userPass[Math.floor(Math.random() * userPass.length)];
+  };
+  return passwordNew;
+};
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
--- README.md
+++ README.md
@@ -1,2 +1,63 @@
 # JS-Password-Homework
+
 Homework 03 - Javascript - Random Password Generator
+
+## Your Task
+
+This week’s homework requires you to modify starter code to create an application that enables employees to generate random passwords based on criteria that they’ve selected. This app will run in the browser and will feature dynamically updated HTML and CSS powered by JavaScript code that you write. It will have a clean and polished, responsive user interface that adapts to multiple screen sizes.
+
+## User Story
+
+```
+AS AN employee with access to sensitive data
+I WANT to randomly generate a password that meets certain criteria
+SO THAT I can create a strong password that provides greater security
+```
+
+## Acceptance Criteria
+
+```
+GIVEN I need a new, secure password
+
+WHEN I click the button to generate a password
+THEN I am presented with a series of prompts for password criteria
+
+WHEN prompted for password criteria
+THEN I select which criteria to include in the password
+
+WHEN prompted for the length of the password
+THEN I choose a length of at least 8 characters and no more than 128 characters
+
+WHEN asked for character types to include in the password
+THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+
+WHEN I answer each prompt
+THEN my input should be validated and at least one character type should be selected
+
+WHEN all prompts are answered
+THEN a password is generated that matches the selected criteria
+
+WHEN the password is generated
+THEN the password is either displayed in an alert or written to the page
+```
+
+## Expected Mock-Up
+
+The following image shows the web application's appearance and functionality:
+
+![The Password Generator application displays a red button to "Generate Password".](./Assets/03-javascript-homework-demo.png)
+
+## Execution Details
+
+First, I broke up the needed steps into pseudo code (also attached in repo as a .txt file)
+After establishing the flow of process, I started to break down the logic through JS.
+
+I realized that by continually adding approved arrays (character types) into an empty array (user password), at the end of the process I could use a for loop to randomly iterate through the final combined array to output our randomized password.
+
+## Deployment Details
+
+Repo: [JS Random Password Generator - Alex Geis Github](https://github.com/alexgeis/JS-Password-Homework)
+
+GitHub Pages URL: [JS Random Password Generator](https://alexgeis.github.io/JS-Password-Homework/)
+
+Screenshot: ![The Password Generator application displays a red button to "Generate Password".](./Assets/password-generator-screenshot.png)
